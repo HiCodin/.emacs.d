42e963ec8c029a46bfaf8232c220807295d37ec7
HiCodin <HiCodin@users.noreply.github.com>
2015-12-09 11:58:56 +0630

    initial comit

---
+++ custom.el
@@ -0,0 +1,30 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(ansi-term-color-vector
+   [unspecified "#151515" "#fb9fb1" "#acc267" "#ddb26f" "#6fc2ef" "#e1a3ee" "#6fc2ef" "#d0d0d0"])
+ '(evil-echo-state nil)
+ '(paradox-github-token t)
+ '(powerline-height 22))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cursor ((t (:background "mediumslateblue"))))
+ '(helm-ff-directory ((t (:foreground "light salmon"))))
+ '(helm-ff-dotted-directory ((t (:foreground "DimGray"))))
+ '(helm-match ((t (:foreground "OrangeRed2"))))
+ '(helm-selection ((t (:background "medium slate blue" :foreground "white"))))
+ '(helm-source-header ((t (:foreground "chocolate1" :weight normal :height 1.2 :width normal :family "Sans Serif"))))
+ '(spaceline-modified ((t (:background "chocolate1" :foreground "White" ))))
+ '(spaceline-read-only ((t (:background "tomato1" :foreground "White"))))
+ '(spaceline-unmodified ((t (:background "LimeGreen" :foreground "White")))))
+
+(setq custom-safe-themes t)
---
+++ init.el
@@ -0,0 +1,28 @@
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+(package-initialize)
+
+;;; Bootstrap use-package
+;; Install use-package if it's not already installed.
+;; use-package is used to configure the rest of the packages.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+
+(org-babel-load-file (concat user-emacs-directory "config.org"))
+
